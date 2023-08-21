691c8bb977df8e3cb6eccddd1b68e0d86ec1de2d
Lokuyow <cg.magnet@gmail.com>
2023-08-21 17:20:21 +0900

    copy

---
+++ main.js
@@ -0,0 +1,517 @@
+const COINGECKO_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=jpy%2Cusd%2Ceur&include_last_updated_at=true&precision=3";
+const satsInBtc = 1e8;
+const inputFields = ['sats', 'btc', 'jpy', 'usd', 'eur'];
+const dateTimeFormatOptions = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit'
+};
+const BASE_URL = "https://lokuyow.github.io/sats-rate/";
+const BASE_TEXTS = {
+    sats: "₿：{value} sats",
+    btc: "₿：{value} BTC",
+    jpy: "¥：{value} JPY",
+    usd: "$：{value} USD",
+    eur: "€：{value} EUR"
+};
+let btcToJpy, btcToUsd, btcToEur, lastUpdatedField;
+let lastUpdatedTimestamp = null;
+let touchStartTime = 0;
+let longPressed = false;
+let touchMoved = false;
+
+document.addEventListener('DOMContentLoaded', initializeApp);
+
+async function initializeApp() { 
+        await fetchDataFromCoinGecko();
+        setupEventListeners();
+        handleServiceWorker();
+        loadValuesFromQueryParams();
+        document.addEventListener('visibilitychange', handleVisibilityChange);
+        window.addEventListener('online', handleOnline);
+}
+
+async function fetchDataFromCoinGecko() {
+    let data;
+
+    if (navigator.onLine) {
+        try {
+            data = await fetch(COINGECKO_URL).then((res) => res.json());
+        } catch (err) {
+            handleCoinGeckoRequestError(err);
+        }
+    } else {
+        data = await caches.open(RATE_CACHE_NAME).then((cache) => {
+            return cache.match(COINGECKO_URL).then((response) => {
+                return response ? response.json() : null;
+            });
+        });
+        alert('オフラインです。過去の価格レートを参照しています。');
+    }
+
+    if (data) {
+        updateCurrencyRates(data);
+        updateLastUpdated(data.bitcoin.last_updated_at);
+    }
+    setDefaultValues();
+    if (lastUpdatedField) {
+        calculateValues(lastUpdatedField);
+    }
+    updateElementClass(getDomElementById('update-prices'), false);
+}
+
+async function getCoinGeckoData() {
+    const response = await fetch(COINGECKO_URL);
+    return response.json();
+}
+
+function setupEventListeners() {
+    inputFields.forEach(id => {
+        const element = getDomElementById(id);
+        setupInputFieldEventListeners(element);
+    });
+    setupEventListenersForCurrencyButtons()
+    getDomElementById('share-via-webapi').addEventListener('click', shareViaWebAPIEvent);
+    getDomElementById('update-prices').addEventListener('click', updateElementsBasedOnTimestamp);
+}
+
+function setupInputFieldEventListeners(element) {
+    element.addEventListener('keyup', handleInputFormatting);
+    element.addEventListener('focus', handleFocus);
+    element.addEventListener('touchstart', handleTouchStart, { passive: true });
+    element.addEventListener('touchmove', handleTouchMove, { passive: true });    
+    element.addEventListener('touchend', handleTouchEnd);
+    element.addEventListener('contextmenu', handleContextMenu);
+}
+
+function getDomElementById(id) {
+    return document.getElementById(id);
+}
+
+function handleOnline() {
+    console.log('オンラインに復帰しました。最新データを取得中です。');
+    fetchDataFromCoinGecko();
+}
+
+function handleCoinGeckoRequestError(err) {
+    console.error("Failed to fetch price data from CoinGecko:", err);
+    alert("価格レートの取得に失敗しました。過去の価格レートを参照しています。時間をおいてからリロードしてみてください。");
+}
+
+function setDefaultValues() {
+    const satsField = getDomElementById('sats');
+    if (!satsField.value) {
+        satsField.value = formatCurrency("100");
+        calculateValues('sats');
+    }
+}
+
+function updateCurrencyRates(data) {
+    btcToJpy = data.bitcoin.jpy;
+    btcToUsd = data.bitcoin.usd;
+    btcToEur = data.bitcoin.eur;
+}
+
+function getInputValue(id) {
+    return getDomElementById(id).value.replace(/,/g, '');
+}
+
+// 計算
+function calculateValues(inputField) {
+    const values = {
+        btc: getInputValue('btc'),
+        sats: getInputValue('sats'),
+        jpy: getInputValue('jpy'),
+        usd: getInputValue('usd'),
+        eur: getInputValue('eur')
+    };
+
+    switch (inputField) {
+        case 'btc':
+            values.sats = values.btc * satsInBtc;
+            values.jpy = values.btc * btcToJpy;
+            values.usd = values.btc * btcToUsd;
+            values.eur = values.btc * btcToEur;
+            break;
+        case 'sats':
+            values.btc = values.sats / satsInBtc;
+            values.jpy = values.btc * btcToJpy;
+            values.usd = values.btc * btcToUsd;
+            values.eur = values.btc * btcToEur;
+            break;
+        case 'jpy':
+            values.btc = values.jpy / btcToJpy;
+            values.sats = values.btc * satsInBtc;
+            values.usd = values.btc * btcToUsd;
+            values.eur = values.btc * btcToEur;
+            break;
+        case 'usd':
+            values.btc = values.usd / btcToUsd;
+            values.sats = values.btc * satsInBtc;
+            values.jpy = values.btc * btcToJpy;
+            values.eur = values.btc * btcToEur;
+            break;
+        case 'eur':
+            values.btc = values.eur / btcToEur;
+            values.sats = values.btc * satsInBtc;
+            values.jpy = values.btc * btcToJpy;
+            values.usd = values.btc * btcToUsd;
+            break;
+        default:
+            console.error("Unknown inputField:", inputField);
+            return;
+    }
+
+    inputFields.forEach(id => {
+        if (id === inputField) {
+            const element = getDomElementById(id);
+            const caretPos = element.selectionStart;
+            element.setSelectionRange(caretPos, caretPos);
+        } else {
+            getDomElementById(id).value = formatCurrency(values[id], id);
+        }
+    });
+
+    lastUpdatedField = inputField;
+    updateShareButton(values.btc, values.sats, values.jpy, values.usd, values.eur);
+}
+
+// キー入力
+function handleInputFormatting(event) {
+    addCommasToInput(event.target);
+}
+
+// 数値入力時のカンマ追加とカーソル位置調整
+function addCommasToInput(inputElement) {
+    const originalCaretPos = inputElement.selectionStart;
+    const originalValue = inputElement.value.replace(/,/g, '');
+
+    let preCommaCount = (inputElement.value.slice(0, originalCaretPos).match(/,/g) || []).length;
+
+    let formattedValue = originalValue;
+    if (!(originalValue.endsWith('.') || (originalValue.includes('.') && originalCaretPos > originalValue.indexOf('.')))) {
+        formattedValue = formatCurrency(originalValue);
+    }
+
+    let postCommaCount = (formattedValue.slice(0, originalCaretPos).match(/,/g) || []).length;
+    let diffCommaCount = postCommaCount - preCommaCount;
+
+    let newCaretPos = originalCaretPos + diffCommaCount;
+    inputElement.value = formattedValue;
+
+    if (newCaretPos < 0) newCaretPos = 0;
+    if (newCaretPos > formattedValue.length) newCaretPos = formattedValue.length;
+
+    inputElement.selectionStart = newCaretPos;
+    inputElement.selectionEnd = newCaretPos;
+}
+
+// カンマ追加と小数点
+function formatCurrency(num, id) {
+    const currencyFormatOptions = {
+        btc: { maximumFractionDigits: 8, minimumFractionDigits: 0 },
+        sats: { maximumFractionDigits: 3, minimumFractionDigits: 0 },
+        jpy: { maximumFractionDigits: 3, minimumFractionDigits: 0 },
+        usd: { maximumFractionDigits: 5, minimumFractionDigits: 0 },
+        eur: { maximumFractionDigits: 5, minimumFractionDigits: 0 }
+    };
+    return Number(num).toLocaleString(undefined, currencyFormatOptions[id]);
+}
+
+// 価格更新日時の表示
+function updateLastUpdated(timestamp) {
+    const updatedAt = new Date(timestamp * 1000);
+    const formatter = new Intl.DateTimeFormat('ja-JP', dateTimeFormatOptions);
+    getDomElementById('last-updated').textContent = formatter.format(updatedAt);
+    lastUpdatedTimestamp = timestamp;
+}
+
+// 更新ボタン
+function handleVisibilityChange() {
+    if (document.hidden) return;
+
+    const diffTime = Math.floor(Date.now() / 1000) - lastUpdatedTimestamp;
+    const updatePricesElement = getDomElementById('update-prices');
+    const lastUpdatedElement = getDomElementById('last-updated');
+
+    updateElementClass(updatePricesElement, diffTime >= 610);
+    updateElementClass(lastUpdatedElement, diffTime >= 610);
+}
+
+async function updateElementsBasedOnTimestamp() {
+    const diffTime = Math.floor(Date.now() / 1000) - lastUpdatedTimestamp;
+
+    if (diffTime >= 610) {
+        await fetchDataFromCoinGecko();
+        const updatedDiffTime = Math.floor(Date.now() / 1000) - lastUpdatedTimestamp;
+
+        const updatePricesElement = getDomElementById('update-prices');
+        const lastUpdatedElement = getDomElementById('last-updated');
+
+        updateElementClass(updatePricesElement, updatedDiffTime >= 610);
+        updateElementClass(lastUpdatedElement, updatedDiffTime >= 610);
+
+        let svg = updatePricesElement.querySelector('svg');
+        if (svg && !svg.classList.contains('rotated')) {
+            svg.classList.add('rotated');
+            svg.addEventListener('animationend', function() {
+                svg.classList.remove('rotated');
+            }, { once: true });
+        }
+    }
+}
+
+function updateElementClass(element, isOutdated) {
+    if (isOutdated) {
+        element.classList.add('outdated');
+        element.classList.remove('recent');
+    } else {
+        element.classList.remove('outdated');
+        element.classList.add('recent');
+    }
+    element.style.visibility = 'visible';
+}
+
+// 選択
+function handleFocus(event) {
+    event.target.select();
+}
+
+function handleTouchStart(event) {
+    touchMoved = false;
+    longPressed = false;
+    longPressTimer = setTimeout(() => {
+        longPressed = true;
+    }, 500);
+}
+
+function handleTouchMove(event) {
+    touchMoved = true;
+    clearTimeout(longPressTimer);
+}
+
+function handleTouchEnd(event) {
+    clearTimeout(longPressTimer);
+}
+
+function handleContextMenu(event) {
+    if (isMobileDevice() && !longPressed) {
+        event.preventDefault();
+    }
+}
+
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+// URLクエリパラメータ
+function loadValuesFromQueryParams() {
+    const urlParams = new URLSearchParams(window.location.search);
+    ['btc', 'sats', 'jpy', 'usd', 'eur'].forEach(field => {
+        if (urlParams.has(field)) {
+            const element = getDomElementById(field);
+            element.value = formatCurrency(urlParams.get(field));
+            calculateValues(field);
+        }
+    });
+}
+function getQueryString(field, value) {
+    return `?${field}=${value.replace(/,/g, '')}`;
+}
+function generateQueryStringFromValues() {
+    if (!lastUpdatedField) return '';
+    const values = getValuesFromElements();
+    return getQueryString(lastUpdatedField, values[lastUpdatedField]);
+}
+
+// インプットフィールドからカンマを取り除いた数値を取得
+function getValuesFromElements() {
+    const values = {};
+    inputFields.forEach(field => {
+        values[field] = getDomElementById(field).value.replace(/,/g, '');
+    });
+    return values;
+}
+
+// 共有テキスト生成
+function generateCopyText(values) {
+    const formattedTexts = inputFields.map(field => BASE_TEXTS[field].replace('{value}', formatCurrency(values[field], field))).join('\n');
+    return `${formattedTexts}\nPowered by CoinGecko,`;
+}
+
+// 共有ボタン
+function updateShareButton(btc, sats, jpy, usd, eur) {
+    const values = { btc, sats, jpy, usd, eur };
+    
+    const shareText = generateCopyText(values);
+    const queryParams = generateQueryStringFromValues();
+
+    const links = generateShareLinks(queryParams, shareText);
+
+    getDomElementById('share-twitter').href = links.twitter;
+    getDomElementById('share-nostter').href = links.nostter;
+    getDomElementById('share-mass-driver').href = links.massDriver;
+}
+
+function generateShareLinks(queryParams, shareText) {
+    const shareUrl = `${BASE_URL}${queryParams}`;
+    return {
+        twitter: `https://twitter.com/share?url=${encodeURIComponent(shareUrl)}&text=${encodeURIComponent(shareText)}`,
+        nostter: `https://nostter.vercel.app/post?content=${encodeURIComponent(shareText)}%20${encodeURIComponent(shareUrl)}`,
+        massDriver: `https://mdrv.shino3.net/?intent=${encodeURIComponent(shareText)}%20${encodeURIComponent(shareUrl)}`
+    };
+}
+
+function setupEventListenersForCurrencyButtons() {
+    ['sats', 'btc', 'jpy', 'usd', 'eur'].forEach(currency => {
+        getDomElementById('copy-' + currency).addEventListener('click', function(event) {
+            copySingleCurrencyToClipboardEvent(event);
+        });
+
+        getDomElementById('paste-' + currency).addEventListener('click', function(event) {
+            pasteFromClipboardToInput(currency);
+        });
+    });
+
+    getDomElementById('copy-to-clipboard').addEventListener('click', copyToClipboardEvent);
+}
+
+// クリップボードにコピー　各通貨
+function copySingleCurrencyToClipboardEvent(event) {
+    const currency = event.target.dataset.currency;
+    const values = getValuesFromElements();
+    copyToClipboard(values[currency], event, 'left');
+}
+
+// クリップボードにコピー　全体
+function copyToClipboardEvent(event) {
+    const values = getValuesFromElements();
+    const baseText = generateCopyText(values);
+    const queryParams = generateQueryStringFromValues();
+    const textToCopy = `${baseText} ${BASE_URL}${queryParams}`;
+    copyToClipboard(textToCopy, event, 'right');
+}
+
+// コピー、ポップアップ表示
+function copyToClipboard(text, event, align = 'right') {
+    navigator.clipboard.writeText(text).then(() => {
+        const notification = getDomElementById('notification');
+        notification.textContent = 'クリップボードにコピーしました';
+        
+        notification.style.left = event.pageX + 'px';
+        notification.style.top = (event.pageY + 20) + 'px';
+        
+        if (align === 'left') {
+            notification.style.transform = 'translateX(0)';
+        } else {
+            notification.style.transform = 'translateX(-100%)';
+        }
+
+        notification.style.visibility = 'visible';
+
+        setTimeout(() => {
+            notification.style.visibility = 'hidden';
+        }, 1000);
+    }).catch(err => {
+        console.error('クリップボードへのコピーに失敗しました', err);
+    });
+}
+
+// クリップボードから読み取り
+async function readFromClipboard() {
+    try {
+        return await navigator.clipboard.readText();
+    } catch (error) {
+        console.error("クリップボードからの読み取りに失敗しました:", error);
+        return null;
+    }
+}
+
+// クリップボードから貼り付け
+async function pasteFromClipboardToInput(currency) {
+    const clipboardData = await readFromClipboard();
+    const numericValue = parseFloat(clipboardData);
+    if (!isNaN(numericValue)) {
+        const formattedValue = formatCurrency(numericValue, currency);
+        getDomElementById(currency).value = formattedValue;
+        calculateValues(currency);
+    } else {
+        console.log("クリップボードの内容が数値ではありません。");
+    }
+}
+
+// Web Share API
+function shareViaWebAPIEvent() {
+    const values = getValuesFromElements();
+    const shareText = generateCopyText(values);
+    const queryParams = generateQueryStringFromValues();
+    shareViaWebAPI(shareText, queryParams);
+}
+
+function shareViaWebAPI(originalShareText, queryParams) {
+    const modifiedShareText = originalShareText.replace(/https:\/\/lokuyow\.github\.io\/sats-rate\/.*$/, '');
+
+    if (navigator.share) {
+        navigator.share({
+            title: 'おいくらサッツ',
+            text: modifiedShareText,
+            url: `https://lokuyow.github.io/sats-rate/${queryParams}`
+        });
+    } else {
+        alert('お使いのブラウザはWeb共有APIをサポートしていません。別のブラウザを試してください。');
+    }
+}
+
+// サービスワーカー
+function handleServiceWorker() {
+    if (!('serviceWorker' in navigator)) return;
+
+    navigator.serviceWorker.register('./sw.js').then(reg => {
+        reg.addEventListener('updatefound', () => {
+            const newWorker = reg.installing;
+            newWorker.addEventListener('statechange', () => {
+                if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+                    notifyUserOfUpdate(reg);
+                }
+            });
+        });
+    });
+}
+function notifyUserOfUpdate(reg) {
+    const updateNotice = document.createElement('div');
+    updateNotice.className = 'update-notice';
+
+    const updateBox = document.createElement('div');
+    updateBox.className = 'update-notice-box';
+    updateNotice.appendChild(updateBox);
+
+    const title = document.createElement('h3');
+    title.innerHTML = 'アップデート通知';
+    updateBox.appendChild(title);
+
+    const text = document.createElement('p');
+    text.innerHTML = '新しいバージョンが利用可能です。';
+    updateBox.appendChild(text);
+
+    const updateButton = document.createElement('button');
+    updateButton.id = 'updateBtn';
+    updateButton.innerHTML = '更新';
+    updateBox.appendChild(updateButton);
+
+    document.body.appendChild(updateNotice);
+
+    getDomElementById('updateBtn').addEventListener('click', () => {
+        if (reg.waiting) {
+            reg.waiting.postMessage('skipWaiting');
+            reg.waiting.addEventListener('statechange', () => {
+                if (reg.waiting == null) {
+                    window.location.reload();
+                }
+            });
+        } else {
+            console.warn('Service Worker is not waiting.');
+        }
+    });
+}
---
+++ sw.js
@@ -0,0 +1,82 @@
+// Cache name
+const CACHE_NAME = 'sats-rate-caches-v1.3';
+const RATE_CACHE_NAME = 'rate-cache-v1';
+const RATE_URL = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=jpy%2Cusd%2Ceur&include_last_updated_at=true&precision=3';
+// Cache targets
+const urlsToCache = [
+  './index.html',
+  './styles.css',
+  './main.js',
+  './favicons/favicon.ico',
+  './images/icon_x192.png',
+  './images/icon_x512.png',
+  './images/maskable_icon_x192.png',
+  './images/maskable_icon_x512.png',
+  './images/title.svg',
+  './images/copy-regular.svg',
+  './images/paste-regular.svg',
+  './images/白抜きのビットコインアイコン.svg',
+  './images/白抜きの円アイコン.svg',
+  './images/白抜きのドルアイコン.svg',
+  './images/白抜きのユーロアイコン.svg',
+  './images/square-x-twitter.svg',
+  './images/nostr-icon-purple-on-white.svg',
+  './images/cloud-solid.svg',
+  './images/share-nodes-solid.svg',
+  './images/clipboard-solid.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes(RATE_URL)) {
+      event.respondWith(
+          caches.open(RATE_CACHE_NAME).then((cache) => {
+              return fetch(event.request)
+                  .then((response) => {
+                      cache.put(event.request, response.clone());
+                      return response;
+                  })
+                  .catch(() => caches.match(event.request));
+          })
+      );
+  } else {
+      event.respondWith(
+          caches
+              .match(event.request)
+              .then((response) => {
+                  return response ? response : fetch(event.request);
+              })
+      );
+  }
+});
+
+self.addEventListener('activate', (event) => {
+  var cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
