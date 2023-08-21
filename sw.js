dcf75d731b4923cb34e669b28f75f2a1d128111b
Lokuyow <cg.magnet@gmail.com>
2023-08-21 21:40:41 +0900

    test6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache name
-const SW_CACHE_NAME = 'sats-rate-caches-v1.30-test5';
+const SW_CACHE_NAME = 'sats-rate-caches-v1.30-test6';
 const RATE_CACHE_NAME = 'rate-cache-v1';
 const RATE_URL = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=jpy%2Cusd%2Ceur&include_last_updated_at=true&precision=3';
 // Cache targets
@@ -64,11 +64,11 @@ self.addEventListener('fetch', (event) => {
             caches
                 .match(event.request)
                 .then((response) => {
-                    return response ? response : fetch(event.request);
+                    return response || new Response("Offline data not available");
                 })
         );
     }
-  });
+});
 
 self.addEventListener('activate', (event) => {
     var cacheWhitelist = [SW_CACHE_NAME];
