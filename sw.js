ac173ea268d1ecefbc70cee5293b1bf0c047d708
Lokuyow <cg.magnet@gmail.com>
2023-08-22 21:12:05 +0900

    test

--- sw.js
+++ sw.js
@@ -93,9 +93,9 @@ self.addEventListener('fetch', (event) => {
                         return response;
                     }
                     return fetch(event.request).then((networkResponse) => {
-                        if (event.request.method === 'GET') {
+                        if (event.request.url.startsWith('http') && event.request.method === 'GET') {
                             cache.put(event.request, networkResponse.clone());
-                        }                        
+                        }                                           
                         return networkResponse;
                     }).catch(() => {
                         return new Response("Offline data not available");
